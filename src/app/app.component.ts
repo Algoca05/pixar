2a5038d61e3634290e075b313d89612668c925db
Alejandro Gómez Capilla <alejandrogomcap@campus.monlau.com>
2025-02-18 18:37:27 +0100

    inicio2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
-import { MovieComponent } from './movie/movie.component';
 import { FooterComponent } from './footer/footer.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ContentComponent, MovieComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
